081e3dd7bf23d14566e21cd988b7237a94d33509
arujanB <aruzhanboranbay02@gmail.com>
2024-01-11 16:17:21 +0600

    Initial Commit

---
+++ Contact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contact
+//
+//  Created by Aruzhan Boranbay on 11.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
